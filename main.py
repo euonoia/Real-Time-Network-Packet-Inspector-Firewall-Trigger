12c551db3fc42315f865a242e9188a78fa2f5a25
Ken Española <revas4192@gmail.com>
2026-05-19 23:59:43 +0800

    revision: changed the print on the main.py

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from scapy.all import sniff
 from src.inspector import packet_inspector
 
 def main():
-    print("[*] Starting IPS Sniffer... Press Ctrl+C to stop.")
-    # Sniff network traffic and pass it to our inspector module
+    print("[*] Real Time Network Packet Inspector Firewall Press Ctrl+C to stop.")
     sniff(prn=packet_inspector, store=0)
 
 if __name__ == "__main__":
